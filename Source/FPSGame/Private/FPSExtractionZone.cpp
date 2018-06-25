2ba2fc4d58161482c81d0051aaa8e5c1ae3c9073
Shawn <shawncc.huang@gmail.com>
2018-06-25 22:48:26 +0800

    Initialize xCode project on Mac

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -29,7 +29,7 @@ void AFPSExtractionZone::BeginPlay()
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, 
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-
+    
 	UE_LOG(LogTemp,Log,TEXT("Over lapped with extraction zone !"));
 }
 
